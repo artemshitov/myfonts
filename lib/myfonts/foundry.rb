25b22838421b2ccc1905e9b000605b76cb08ea87
Artem Shitov <inbox@artemshitov.ru>
2012-12-20 18:55:53 +0400

    Just inherit foundry from designer since they have the same look and behavior

--- lib/myfonts/foundry.rb
+++ lib/myfonts/foundry.rb
@@ -1,33 +1,6 @@
-require "myfonts/model"
+require "myfonts/designer"
 
 module MyFonts
-  class Foundry < Model
-    def initialize(url, name=nil)
-      @name = name
-      super(url)
-    end
-
-    def name
-      @name ||= get_name
-    end
-
-    def families
-      @families ||= get_families
-    end
-
-    private
-
-    def get_name
-      dom.css("title").text.gsub(/ . MyFonts/, "")
-    end
-
-    def get_families
-      result = dom.css("h4 a").select do |a|
-        a.get_attribute("href") =~ /^\/fonts/
-      end
-      result.map do |a|
-        Family.new("http://www.myfonts.com" + a.get_attribute("href"), a.text)
-      end
-    end
+  class Foundry < Designer
   end
 end
